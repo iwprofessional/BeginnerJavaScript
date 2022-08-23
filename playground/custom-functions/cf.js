f8888ff0c7a943d5b48400f63684accf31e4e743
Ian Wood <ian.wood.professional@gmail.com>
2022-08-22 20:10:00 -0400

    Test20

--- playground/custom-functions/cf.js
+++ playground/custom-functions/cf.js
@@ -1,1 +1,11 @@
-console.log('it works');
+
+
+
+function calculateBill(billAmount, taxRate) {
+   console.log('Running Function'); 
+   const total = billAmount * 1 + taxRate;
+   return total;
+}
+const mytotal = calculateBill(100, 0.13);
+const mytotal2 = calculateBill(50, 0.13);
+console.log(mytotal, mytotal2);
